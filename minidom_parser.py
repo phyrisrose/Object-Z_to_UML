f3c3150314f399ce75d23744a8c7d46fd8aed90b
Anna Andriyanova <phyrisrose@gmail.com>
2012-04-17 13:30:43 -0600

    Moar handlers.

--- minidom_parser.py
+++ minidom_parser.py
@@ -36,20 +36,71 @@ class XMLParser(object):
             # There may be more than one name there
             # When we figure out how to unescape CDATA, we need to break it down
             # for now, the gnarly string is the name of the object
-            btd.name = self.handleStructName(typeDef.getElementsByTagName('name')[0])
+            btd.name = self.handleName(typeDef.getElementsByTagName('name')[0])
             btd.type = 'basicTypeDef'
             logging.info('New basicTypeDef: %s' % btd)
             self.generated.append(btd)
 
+    # TODO: Figure out how to ignore tags that aren't used in the file being parsed.
     def handleFreeTypeDef(self, freeTypeDef):
-        logging.info("should break here")
+        logging.info("should break here, freeTypeDef is undefined in the current doc")
 
     def handleClassDef(self, classDef):
         cls = BasicClass()
-        cls.name = self.handleStructName(classDef.getElementsByTagName('name')[0])
+        cls.name = self.handleName(classDef.getElementsByTagName('name')[0])
         cls.type = 'class'
         # CLASS CHILDREN HANDLERS HERE
+        self.handleState(classDef.getElementsByTagName('state'))
+        self.handleInitState(classDef.getElementsByTagName('initialState')[0])
+        ftns_to_append = self.handleOperations(classDef.getElementsByTagName('operation'))
+        cls.functions.append(ftns_to_append)
+        # by this point, the class entity should be complete
         self.generated.append(cls)
 
-    def handleStructName(self, name):
+    def handleName(self, name):
         return self.getCDATA(name.childNodes)
+
+    def handleState(self, states):
+        for state in states:
+            # do something with state
+            self.handleStateDeclaration(state.getElementsByTagName('declaration')[0])
+            self.handleStatePredicate(state.getElementsByTagName('predicate')[0])
+
+    def handleInitState(self, initialState):
+        pass
+
+    # it's likely that a class will have several operations,
+    # so we have a dedicated function to iterate through all.
+    def handleOperations(self, operations):
+        functions = []
+        for operation in operations:
+            functions.append(self.handleOperation(operation))
+        return functions
+
+    def handleOperation(self, operation):
+        ftn = Function()
+        ftn.name = self.handleName(operation.getElementsByTagName('name')[0])
+        self.handleOpDeltaList(operation.getElementsByTagName('deltaList')[0])
+        self.handleOperationDeclaration(operation.getElementsByTagName('declaration')[0])
+        self.handleOperationPredicate(operation.getElementsByTagName('predicate')[0])
+        # assume the previous functions gathered some data, now we
+        # ftn.append_some_attributes_to_the_object
+        # and pass it up to handleOperations()
+        return ftn
+
+    # From what I gather, state and operation declarations and predicates
+    # serve different purposes. So we need a special one for each
+    def handleStateDeclaration(self, declaration):
+        pass
+
+    def handleOperationDeclaration(self, declaration):
+        pass
+
+    def handleStatePredicate(self, predicate):
+        pass
+
+    def handleOperationPredicate(self, predicate):
+        pass
+
+    def handleOpDeltaList(self, deltaList):
+        pass
