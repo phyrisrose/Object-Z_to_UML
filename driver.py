5e80e8296ff0abc97490d32253813ed79d2b9662
Sam Sorensen <sabrsorensen@gmail.com>
2012-05-01 17:09:49 -0600

    Handle passing of input file as program argument (no longer hardcoded)

--- driver.py
+++ driver.py
@@ -4,12 +4,15 @@ __date__ = 'Spring 2012'
 from xml_parser import XMLParser
 from uml_builder import UMLBuilder
 
-parser = XMLParser('sample.xml')
-builder = UMLBuilder(parser.classes_list, parser.relations_list, parser.types_list)
-print "##########"
-print "Lists:"
-print "Classes found: %s" % parser.classes_list
-print "Relations found: %s" % parser.relations_list
-print "Types found: %s" % parser.types_list
-print "##########"
-builder.gen_uml('uml.uxf')
+if sys.argv:
+    parser = XMLParser(sys.argv[1])
+    builder = UMLBuilder(parser.classes_list, parser.relations_list, parser.types_list)
+    print "##########"
+    print "Lists:"
+    print "Classes found: %s" % parser.classes_list
+    print "Relations found: %s" % parser.relations_list
+    print "Types found: %s" % parser.types_list
+    print "##########"
+    builder.gen_uml('uml.uxf')
+else:
+    print "No input file specified. Please specify the location of a TOZE Object-Z specification as argument 1."
